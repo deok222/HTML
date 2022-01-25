9a761c5f01bfcc8864f0e34381bf8ccaaa273701
deok222 <crarus56@gmail.com>
2022-01-25 11:57:45 +0900

    add video

--- everest/js/common.js
+++ everest/js/common.js
@@ -3,10 +3,10 @@ const menu = document.querySelector(".menu");
 // const close = btn.querySelector("i"); 아래와 같은 뜻의 코드
 const close = document.querySelector(".bars i");
 
-// uil-multiply
-
 btn.addEventListener("click", () => {
   console.log("cliked!!~!!");
   menu.classList.toggle("active");
   close.classList.toggle("uil-multiply");
 });
+
+// js는 css랑 조금 다른 개념으로 HTML에 엮이지 않고 독자적인 부분이 있다.
